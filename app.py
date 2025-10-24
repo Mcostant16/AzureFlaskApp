03220277b90fdc5bed1b6e229fd9e9cd6ce2f558
Costantino <mecostantino@pstcc.edu>
2025-10-24 15:58:45 -0400

    Updated App File

--- app.py
+++ app.py
@@ -8,5 +8,4 @@ def home():
     return "Hello from Flask on Azure!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
